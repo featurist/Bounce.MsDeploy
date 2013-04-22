fca21d3e0d064305b081cfeb06efdeecf1871967
Tim Macfarlane <timmacfarlane@gmail.com>
2013-04-22 11:55:58 +0100

    make msdeploy not mental

---
+++ Bounce.MsDeploy/ExecArgsBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Bounce.MsDeploy
+{
+    internal class ExecArgsBuilder
+    {
+        private List<string> _args = new List<string>();
+
+        public void Add(string format, params object[] args)
+        {
+            _args.Add(string.Format(format, args));
+        }
+
+        public string Args
+        {
+            get { return string.Join(" ", _args); }
+        }
+    }
+}
---
+++ Bounce.MsDeploy/MsDeployPackage.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Bounce.Config;
+using Bounce.Framework;
+using FS;
+
+namespace Bounce.MsDeploy
+{
+    public class MsDeployPackage
+    {
+        private readonly ILog _log;
+        private readonly IShell _shell;
+        private readonly TemplateConfigurer _config;
+
+        public MsDeployPackage(ILog log, IShell shell, TemplateConfigurer config)
+        {
+            _log = log;
+            _shell = shell;
+            _config = config;
+        }
+
+        public void Deploy(string package, string webProject, Dictionary<string, object> environment, string username = "", string password = "")
+        {
+            var archive = ConfiguredMsDeployArchiveDirectory(package, webProject, environment);
+            var servers = Servers(environment);
+
+            foreach (var server in servers)
+            {
+                var site = environment["site"];
+                _log.Info("deploying to server: {0}, site: {1}", server, site);
+
+                var args = new ExecArgsBuilder();
+                args.Add("-source:archiveDir='{0}'", archive);
+                args.Add(
+                    "-dest:auto,computerName='http://{0}/MSDeployAgentService',includeAcls='False',username='{1}',password='{2}',authtype=ntlm",
+                    server, username, password);
+                args.Add("-verb:sync");
+                args.Add("-disableLink:AppPoolExtension");
+                args.Add("-disableLink:ContentExtension");
+                args.Add("-disableLink:CertificateExtension");
+                args.Add(@"-setParam:""IIS Web Application Name""=""{0}""", site);
+                _shell.Exec(@"msdeploy", args.Args);
+            }
+        }
+
+        private string ConfiguredMsDeployArchiveDirectory(string zipPackage, string webProject, Dictionary<string, object> environment)
+        {
+            var archiveDir = CreateTemporaryDirectory();
+            ExtractZipFile(zipPackage, archiveDir);
+
+            var configFiles = ConfigFilesIn(archiveDir);
+            var templateFile = Path.Combine(webProject, "web.template.config");
+            _config.ConfigureFiles(templateFile, environment, configFiles);
+
+            RemoveWebConfigParameters(archiveDir);
+
+            return archiveDir;
+        }
+
+        private void RemoveWebConfigParameters(string archiveDir)
+        {
+            var parametersFile = Path.Combine(archiveDir, "parameters.xml");
+            var parameters = XDocument.Load(parametersFile);
+            var xmlParameterEntries = ((IEnumerable)parameters.Root.XPathEvaluate(@"//parameter[parameterEntry/@kind=""XmlFile""]")).Cast<XElement>();
+
+            foreach (var xmlParameterEntry in xmlParameterEntries)
+            {
+                _log.Info("removing msdeploy package parameter: " + xmlParameterEntry.Attribute("name").Value);
+                xmlParameterEntry.Remove();
+            }
+
+            parameters.Save(parametersFile);
+        }
+
+        private string CreateTemporaryDirectory()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+
+        private static void ExtractZipFile(string zipPackage, string archive)
+        {
+            new ICSharpCode.SharpZipLib.Zip.FastZip().ExtractZip(zipPackage, archive, null);
+        }
+
+        private static IEnumerable<string> ConfigFilesIn(string archive)
+        {
+            return new FileSystem().Find(archive).Where(IsWebConfig);
+        }
+
+        private static bool IsWebConfig(string path)
+        {
+            return Path.GetFileName(path).Equals("Web.config", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static IEnumerable<string> Servers(Dictionary<string, object> environmentVariables)
+        {
+            var environmentVariable = environmentVariables.ContainsKey("servers")
+                                          ? environmentVariables["servers"]
+                                          : environmentVariables["server"];
+            return environmentVariable.ToString().Split(',').Select(s => s.Trim());
+        }
+    }
+}
